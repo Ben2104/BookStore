1656c9bd3a22b126aa1e2cf6f9390515ae7eef0d
Ben2104 <dohoangkhoi341@gmail.com>
2024-12-31 12:35:46 -0800

    first upload

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555
+
+export const mongoDBURL = 
+"mongodb+srv://dohoangkhoi341:Khoidenkhoi123*@books-store-mearn.wmeu9.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MEARN"
